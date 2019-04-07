d81daf4b8f1df7a962d65166d3db0c32266f7803
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2019-04-08 06:20:19 +0900

    comment

--- src/TypeReconstruction.hs
+++ src/TypeReconstruction.hs
@@ -1,11 +1,12 @@
 module TypeReconstruction where
 
-import Prelude hiding (Integer)
-import Data.Map.Strict (Map)
+import           Prelude         hiding (Integer)
+
+import qualified Data.List       as List
+import           Data.Map.Strict (Map)
 import qualified Data.Map.Strict as Map
-import Data.Set (Set)
-import qualified Data.Set as Set
-import qualified Data.List as List
+import           Data.Set        (Set)
+import qualified Data.Set        as Set
 
 -- #@@range_begin(term)
 data Term
@@ -39,34 +40,44 @@ type VariableName = String
 -- #@@range_end(other_type)
 
 -- | 型から自由型変数を抽出する
-freeVarialbes :: Type -> Set VariableName
-freeVarialbes (TypeVariable x) = Set.singleton x
-freeVarialbes Integer = Set.empty
-freeVarialbes Boolean = Set.empty
-freeVarialbes (Arrow t1 t2) = Set.union (freeVarialbes t1) (freeVarialbes t2)
+freeVariables :: Type -> Set VariableName
+freeVariables (TypeVariable x) = Set.singleton x
+freeVariables Integer = Set.empty
+freeVariables Boolean = Set.empty
+freeVariables (Arrow t1 t2) = Set.union (freeVariables t1) (freeVariables t2)
 
 -- #@@range_begin(unify)
 -- | 単一化関数
 unify :: Constraint -> Either String [Assign]
 unify c
+  -- 制約が空の場合は代入のリストも空である
   | Set.null c = pure []
   | otherwise =
       let
+        -- 制約 c から1つ等式を取り出す
         eq = Set.elemAt 0 c
+        -- 残りの制約を c' とする
         c' = Set.deleteAt 0 c
       in
         case eq of
+          -- 両辺が同一なら無視する
           (s, t) | s == t -> unify c'
-          (TypeVariable x, t) | Set.notMember x (freeVarialbes t) -> do
+          -- 左辺が型変数でそれが右辺の自由変数でないとき
+          (TypeVariable x, t) | Set.notMember x (freeVariables t) -> do
             let a = (x, t)
+            -- 制約に代入し再帰的に単一化する
             as <- unify (assignConstraint [a] c')
+            -- 効率のため代入のリストは数式と左右反転させている
             pure (a:as)
-          (s, TypeVariable x) | Set.notMember x (freeVarialbes s) -> do
+          -- 右辺が変数のときも同様
+          (s, TypeVariable x) | Set.notMember x (freeVariables s) -> do
             let a = (x, s)
             as <- unify (assignConstraint [a] c')
             pure (a:as)
+          -- 両辺が関数型のときは分解し制約に加え再帰的に単一化する
           (Arrow s1 s2, Arrow t1 t2) ->
             unify (Set.insert (s1, t1) (Set.insert (s2, t2) c'))
+          -- それ以外のときはエラーメッセージを返す
           _ -> Left "invalid constraints"
 -- #@@range_end(unify)
 
@@ -92,7 +103,12 @@ assignConstraint as cst =
 
 -- #@@range_begin(constraint_type)
 -- | 制約型付け
-constraintType :: Context -> Term -> [VariableName] -> Either String (Type, Constraint, [VariableName])
+constraintType
+  :: Context -- ^ 文脈
+  -> Term -- ^ 項
+  -> [VariableName] -- ^ 未使用変数名のリスト
+  -> Either String (Type, Constraint, [VariableName])
+  -- ^ 失敗時は文字列、成功時は型・制約・未使用の変数名のリスト
 -- CT-Int
 constraintType _ (IntegerValue _) names = pure (Integer, Set.empty, names)
 -- CT-True, CT-False
@@ -100,25 +116,36 @@ constraintType _ (BooleanValue _) names = pure (Boolean, Set.empty, names)
 -- CT-Var
 constraintType ctx (ValueVariable x) names = do
   typ <-
+    -- 文脈から変数に対応する型を探す
     case Map.lookup x ctx of
       Just typ' -> pure typ'
-      Nothing -> Left ("the context (" <> show (Map.toList ctx) <> ") doesn't have type variable (" <> x <> ")")
+      -- なければエラーメッセージを返す
+      Nothing ->
+        Left ("the context (" <> show (Map.toList ctx)
+              <> ") doesn't have type variable (" <> x <> ")")
   pure (typ, Set.empty, names)
 -- CT-Plus
 constraintType ctx (Plus t1 t2) names = do
+  -- オペンランドに対して再帰的に適用する
   (typ1, cst1, names1) <- constraintType ctx t1 names
   (typ2, cst2, names2) <- constraintType ctx t2 names1
-  let cst = Set.unions [cst1, cst2, Set.fromList [(typ1, Integer), (typ2, Integer)]]
+  let
+    -- 制約の和を取る
+    cst =
+      Set.unions [cst1, cst2, Set.fromList [(typ1, Integer), (typ2, Integer)]]
   pure (Integer, cst, names2)
 -- CT-If
 constraintType ctx (If t1 t2 t3) names = do
   (typ1, cst1, names1) <- constraintType ctx t1 names
   (typ2, cst2, names2) <- constraintType ctx t2 names1
   (typ3, cst3, names3) <- constraintType ctx t3 names2
-  let cst = Set.unions [cst1, cst2, cst3, Set.fromList [(typ1, Boolean), (typ2, typ3)]]
+  let
+    cst =
+      Set.unions [cst1, cst2, cst3, Set.fromList [(typ1, Boolean), (typ2, typ3)]]
   pure (typ2, cst, names3)
 -- CT-Abs
 constraintType ctx (Abstract x t1) names = do
+  -- 未使用の変数名から型変数の型を作り、それと残った変数名のリストを得る
   (xt, names0) <- newTypeVariable names
   let ctx0 = Map.insert x xt ctx
   (typ1, cst1, names1) <- constraintType ctx0 t1 names0
@@ -134,4 +161,4 @@ constraintType ctx (Application t1 t2) names = do
 
 newTypeVariable :: [VariableName] -> Either String (Type, [VariableName])
 newTypeVariable (n:names) = pure (TypeVariable n, names)
-newTypeVariable [] = Left "no unused type variables"
+newTypeVariable []        = Left "no unused type variables"
